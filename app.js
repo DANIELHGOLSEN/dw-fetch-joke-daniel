ed8e927be7d714e70408e1cb0141ed2a465ace9f
daniel <danielolsen030619@gmail.com>
2026-09-15 13:18:18 +0200

    Første metode gennemført.

---
+++ app.js
@@ -0,0 +1,18 @@
+const baseUrl = "https://icanhazdadjoke.com"
+const divElm = document.querySelector(".joke__div")
+
+// fetch(baseUrl, {
+//     headers: {
+//         Accept: "application/json"
+//     }
+// })
+//     .then(result => result.json())
+//     .then(data => {
+//         divElm.innerHTML = `
+//     <li>
+//         <p>${data.joke}</p>
+//     </li>
+//     `
+//     })
+
+
