f4c6663a89c365a7eb2b3af092fa1b5a87fc9c7c
daniel <danielolsen030619@gmail.com>
2026-09-15 13:52:10 +0200

    Opgave færdig.

--- app.js
+++ app.js
@@ -16,3 +16,26 @@ const divElm = document.querySelector(".joke__div")
 //     })
 
 
+async function getData() {
+    try {
+        const response = await fetch(baseUrl, {
+            headers: {
+                Accept: "application/json"
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error("Du gay.")
+        }
+
+        const result = await response.json()
+        divElm.innerHTML = `
+        <li>
+            <p>${result.joke}</p>
+        </li>
+        `
+    } catch (error) {
+        console.log(error)
+    }
+}
+getData()
